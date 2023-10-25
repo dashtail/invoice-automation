2ac72ba0be5e19f645b5cc7d262bf7e90d708e74
Rafael Cabral <rafaelbkbral@gmail.com>
2023-10-25 20:06:19 -0300

    add create_invoice task for every hour

--- app/tasks/scheduler.py
+++ app/tasks/scheduler.py
@@ -4,7 +4,7 @@ from app.tasks.invoice import create_invoices
 scheduler = BackgroundScheduler()
 
 def start_scheduler():
-    # scheduler.add_job(create_invoices, 'interval', hours=3)
+    scheduler.add_job(create_invoices, 'interval', hours=1)
     scheduler.start()
 
 def stop_scheduler():
